ecb732d5c8f55bf20310433f4c325488556d40be
shariliendor <cheddargarden383@gmail.com>
2025-03-09 23:52:30 -0700

    added rest of story, still need to add image choosing

--- CSCI202/FinalProject/js/game.js
+++ CSCI202/FinalProject/js/game.js
@@ -5,8 +5,11 @@ let dialogueIndex = -1;
 
 let buttonX = 300;
 let buttonY = 300;
+let buttonSize = 200;
 let buttonXSpeed = 1;
 let buttonYSpeed = 1;
+let randomizePosOnClick = false;
+let shrinkOnClick = false;
 
 let ignoreNextClick = false;
 
@@ -72,19 +75,77 @@ const dialogues = {
     "1interlude1": [
         "YOU: Wow, did you see that?",
         "LARRY: Yeah, the whole background changed color!",
-        "LARRY: See Carl? Buttons are so much cooler when you push them!"
+        "LARRY: See Carl? Buttons are so much cooler when you push them!",
+        "CARL: NOOOO! You've made it do something!",
+        "YOU: Umm yeah, that's kinda the point...",
+        "CARL: Nopushius will find you!!"
+    ],
+
+    "1interlude2": [
+        "???: Everything alright over here?",
+        "CARL: Joe, thank goodness you're here! They've pushed the button!",
+        "LARRY: Oh come on, not another one!"
+    ],
+
+    "1interlude2.5": [
+        "JOE: What!? No! You told them not to, didn't you?",
+        "CARL: I did! but did they listen?",
+        "YOU: Look, buttons are meant to be pushed",
+        "YOU: You can't get anywhere without pushing buttons!",
+        "LARRY: I guess we'll have to push the button to show this guy too",
+        "LARRY: This was not how I envisioned my day going..."
+    ],
+
+    "1interlude3": [
+        "CARL: NOOOOOO! I CAST DVD LOGO!!!",
+        "JOE: NOOOOOO! I CAST JUMP AFTER PUSH!!!"
+    ],
+
+    "2interlude1": [
+        "JOE: WHAT? They cracked both of our spells together??",
+        "CARL: That's never happened before!",
+        "LARRY: I think you're kinda missing the point here...",
+        "YOU: Yeah... we pushed the button and nobody died",
+        "YOU: It only asked for our names",
+        "JOE: Now Nopushius knows your names,",
+        "JOE: He'll surely be coming for you",
+        "LARRY: Yeah, yeah",
+        "LARRY: Anyone else we need to show that pushing a button isn't the end of the world?",
+        "CARL: I'm calling Amanda, this is getting out of hand",
+        "YOU: Wait seriously!?",
+        "LARRY: How many more of you are there??",
+    ],
+
+    "2interlude2": [
+        "AMANDA: You'd be surprised",
+        "YOU: How did you get here so fast??",
+        "AMANDA: Nopishius sent me. He's on his way here as we speak",
+        "YOU: OK, this is actually getting crazy,",
+        "YOU: Maybe we don't wanna mess with these people",
+        "YOU: for all we know, this is a dangerous gang and they call their leader Nopushius",
+        "LARRY: You might be right,",
+        "LARRY: But if moving a button around is all their henchmen can do,",
+        "LARRY: I seriously doubt we're in trouble",
+        "YOU: Fair. Let's push that button one more time"
+    ],
+
+    "2interlude3": [
+        "CARL: NOOOOOO! I CAST DVD LOGO!!!",
+        "JOE: NOOOOOO! I CAST JUMP AFTER PUSH!!!",
+        "AMANDA: NOOOOOO! I CAST SHRINK AFTER PUSH!!!"
     ]
 };
 
 let buttonMoveFunctions = {
     "DVDLogo": moveButtonDVDLogo,
-    "SidetoSide": moveButtonSidetoSide,
-    "StayStill": moveButtonStayStill
+    //"SidetoSide": moveButtonSidetoSide,
+    //"StayStill": moveButtonStayStill
 };
 
 let queueIndex = -1;
 
 let eventQueue = [
+    // start of game
     "playDialogue startGame1",
     "setVisible button 1",
     "playDialogue startGame2",
@@ -96,10 +157,36 @@ let eventQueue = [
     "setDudePortrait images/carl.jpg",
     "playDialogue startGame5",
 
+    // clicking 1
     "playButtonClicking DVDLogo 10 1500",
     "changeBgColor",
     "playDialogue 1interlude1",
+    "setDudePortrait images/joe.jpg",
+    "playDialogue 1interlude2",
+    "setDudePortrait images/joeAngry.jpg",
+    "playDialogue 1interlude2.5",
+    "setVisible button 1",
+    "waitForClick",
+    "playDialogue 1interlude3",
 
+    // clicking 2
+    "setRandomizePos 1",
+    "playButtonClicking DVDLogo 10 2500", //1500",
+    "getUserName",
+    "playDialogue 2interlude1",
+    "setDudePortrait images/amanda.jpg",
+    "playDialogue 2interlude2",
+    "setVisible button 1",
+    "waitForClick",
+    "playDialogue 2interlude3",
+
+    //clicking 3
+    "setRandomizePos 1",
+    "setShrinking 1",
+    "playButtonClicking DVDLogo 10 3000",
+    // choose image
+
+    // end of game
     "displayFinalProduct"
 ];
 
@@ -155,6 +242,7 @@ function advanceEventQueue() {
         
         case "waitForClick":
             waitingForClick = true;
+            setButtonPosition(300, 300);
             return;
         
         case "playDialogue":
@@ -164,6 +252,10 @@ function advanceEventQueue() {
         
         case "changeBgColor":
             changeBgColor();
+            return;
+        
+        case "getUserName":
+            getUserName();
             return;
         
         case "setDudePortrait":
@@ -183,6 +275,16 @@ function advanceEventQueue() {
             setVisible(elementID, vis);
             advanceEventQueue();
             return;
+        
+        case "setRandomizePos":
+            setRandomizePos(Boolean(Number(words[1])));
+            advanceEventQueue();
+            return;
+        
+        case "setShrinking":
+            shrinkOnClick = Boolean(Number(words[1]));
+            advanceEventQueue();
+            break;
         
         case "displayFinalProduct":
             displayFinalProduct();
@@ -210,20 +312,61 @@ function displayFinalProduct() {
             Do you see what you've done? <br>
             These crimes will not go unpunished!
         </p>
+        
     `;
-
+    //<button id="resetButton">Replay</button>
     setDudePotrait(favoriteImage);
     let dudePortrait = document.getElementById("dudePortrait");
     dudePortrait.style.top = "300px";
+
+    let resetButton = document.getElementById("resetButton");
+    resetButton.addEventListener("click", resetGame);
 }
 
-// function getDialogue(d) {
-//     fetch('json/dialogues.json')
-//         .then(response => response.json())
-//         .then(data => {
-//             return data[d];
-//         })
-//         .catch(error => console.error('Error loading data:', error)); 
+// function resetGame() {
+//     queueIndex = -1;
+
+//     dialogueIndex = -1;
+
+//     buttonX = 300;
+//     buttonY = 300;
+//     buttonSize = 200;
+//     buttonXSpeed = 1;
+//     buttonYSpeed = 1;
+//     randomizePosOnClick = false;
+//     shrinkOnClick = false;
+
+//     ignoreNextClick = false;
+
+//     clickCount = 0;
+//     waitingForClick = false;
+//     clickTimerStart = 0;
+//     clickTimer = 0;
+//     clicksNeeded = 0;
+
+//     playerName = "[player name]";
+//     favoriteImage = "images/carlBPA.jpg";
+//     favoriteColor = "white";
+//     totalButtonPushes = 0;
+
+//     let body = document.getElementById("body");
+//     body.style.backgroundColor = "white";
+    
+
+//     body.innerHTML = `
+//         <div id="wrapper">
+//             <h1 id="text">Hey look a button!</h1>
+//             <div id="button"></div>
+//             <img id="dudePortrait" src="" alt="">
+//             <p id="DialogueBox"></p>
+
+//             <div id="formWrapper"></div>
+        
+//         </div>
+//     `;
+
+
+//     advanceEventQueue();
 // }
 
 function playButtonClicking(buttonMoveFunction, clicksToPass, time) {
@@ -233,10 +376,16 @@ function playButtonClicking(buttonMoveFunction, clicksToPass, time) {
     clickTimer = time;
     clicksNeeded = clicksToPass;
     timerInterval = setInterval(decrementTimer, 1);
+    setVisible("dudePortrait", false);
     
-    // reset button position
-    buttonX = 300;
-    buttonY = 300;
+    // reset button position and size
+    setButtonPosition(300, 300);
+    buttonSize = 200;
+}
+
+function setButtonPosition(x, y) {
+    buttonX = x;
+    buttonY = y;
     updateButtonPosition();
 }
 
@@ -257,6 +406,8 @@ function decrementTimer() {
 
         // done, move on
         setVisible("button", false);
+        buttonSize = 200;
+        setVisible("dudePortrait", true);
         advanceEventQueue();
     }
 }
@@ -270,16 +421,47 @@ function clickButton() {
         ignoreNextClick = true;
         advanceEventQueue();
     }
+
+    if (randomizePosOnClick) {
+        buttonX = Math.floor(Math.random() * (screen.width - buttonSize));
+        buttonY = Math.floor(Math.random() * (screen.height - buttonSize - 175));
+    }
+
+    if (shrinkOnClick) {
+        let button = document.getElementById("button");
+
+        buttonSize -= 10;
+        if(buttonSize <= 0) {
+            buttonSize = 10;
+        }
+
+        button.style.width = buttonSize + "px";
+        button.style.height = buttonSize + "px";
+    }
 }
 
 function updateScoreDisplay() {
     displayText("Time Left: " + clickTimer + " Clicks: " + clickCount + "/" + clicksNeeded, "text");
 }
 
+function getUserName() {
+    formWrapper = document.getElementById("formWrapper");
+    formWrapper.innerHTML = `
+            <label for="nameForm">What's your name? </label>
+            <input type="text" id="nameForm">
+            <button id="formSubmit">Confirm</button>`;
+
+    let nameForm = document.getElementById("nameForm");
+    nameForm.addEventListener("change", setUserName);
+
+    let submitButton = document.getElementById("formSubmit");
+    submitButton.addEventListener("click", removeForm);
+}
+
 function changeBgColor() {
     formWrapper = document.getElementById("formWrapper");
     formWrapper.innerHTML = `
-            <label for="colorPicker">Choose a backgroud color for your website:</label>
+            <label for="colorPicker">Choose a color for the background: </label>
             <input type="color" id="colorPicker">
             <button id="formSubmit">Confirm</button>`;
 
@@ -288,6 +470,10 @@ function changeBgColor() {
 
     let submitButton = document.getElementById("formSubmit");
     submitButton.addEventListener("click", removeForm);
+}
+
+function setUserName(event) {
+    playerName = event.target.value;
 }
 
 function setBgColor(event) {
@@ -317,18 +503,13 @@ function clearButtonMovement() {
 
 function moveButtonStayStill(){}
 
+function setRandomizePos(b) {
+    randomizePosOnClick = b;
+}
+
 function moveButtonDVDLogo() {
     buttonX += buttonXSpeed;
     buttonY += buttonYSpeed;
-
-    if (buttonX <= 0 || (buttonX + 200 >= screen.width)) buttonXSpeed *= -1;
-    if (buttonY <= 0 || (buttonY + 375 >= screen.height)) buttonYSpeed *= -1;
-
-    updateButtonPosition();
-}
-
-function moveButtonSidetoSide() {
-    buttonX += buttonXSpeed;
 
     if (buttonX <= 0 || (buttonX + 200 >= screen.width)) buttonXSpeed *= -1;
     if (buttonY <= 0 || (buttonY + 375 >= screen.height)) buttonYSpeed *= -1;
