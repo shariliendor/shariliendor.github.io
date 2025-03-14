66416f817ff42a708477a7f341b639347a622c33
shariliendor <cheddargarden383@gmail.com>
2025-03-13 23:37:08 -0700

    finished portfolio

--- displayCollection.js
+++ displayCollection.js
@@ -1,46 +1,42 @@
-document.addEventListener('DOMContentLoaded', displayCards);
+document.addEventListener('DOMContentLoaded', start);
 
-function getSites(fileName) {
-    sites = [];
+function start() {
+    displayCards("sites.json", "projectContainer", "Project: ");
+    displayCards("interestingWebsites.json", "interestingContainer", "Interesting Site: ")
+}
+
+function displayCards(fileName, containerID, prefix) {
+    const parent = document.getElementById(containerID);
+  
     fetch(fileName)
         .then(response => response.json())
         .then(data => {
-            const parent = document.getElementById('container');
-            
             data.forEach(item => {
-                sites.push([`${item.title}`, `${item.url}`, `${item.description}`])
-            });
-        })
-        .catch(error => console.error('Error loading data:', error)); 
-
-    return sites;
-}
-
-function displayCards() {
-
-    mySites = getSites("sites.json");
-    interestingSites = getSites("interestingWebsites.json");
-  
-    fetch('sites.json')
-        .then(response => response.json())
-        .then(data => {
-            const parent = document.getElementById('container');
-            
-            data.forEach(item => {
-                addElement(item, parent);
+                addElement(item, parent, prefix);
             });
         })
         .catch(error => console.error('Error loading data:', error)); 
 }
 
-function addElement(item, parent) {
+function addElement(item, parent, prefix) {
     const card = document.createElement('div');
     
     card.setAttribute("class", "card m-1");
-    card.innerHTML = `
-        <h2><a href="${item.url}">${item.title}<a></h2>
-        <p>${item.description}<p>
-        `;
+    card.innerHTML =
+        "<h2><a href='" + item.url + "' target='_blank'>" + prefix + item.title + " <a></h2>" + 
+        "<p>" + item.description + "<p>";
     
     parent.appendChild(card);
+}
+
+function getTitle(site) {
+    return site[0];
+}
+
+function getUrl(site) {
+    return site[1];
+}
+
+function getDesc(site) {
+    return site[2];
 }
